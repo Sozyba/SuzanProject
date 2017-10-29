eae262fd63fc5957831f6885eeee9eac24f2e4ee
user <sozy.31.1.2000@hotmail.com>
2017-10-29 11:39:27 +0200

    Initial commit

---
+++ app/src/main/java/com/badran/suzan/suzanproject/signUpActivity.java
@@ -0,0 +1,19 @@
+package com.badran.suzan.suzanproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class signUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+ private EditText UserName;
+ private EditText Password;
+ private EditText Singin;
+ private Button SingUp;
+private 
